4c1ab1137fafea9b8f0998e09899a41916a1519e
20255816 <mediafire321ea@gmail.com>
2025-10-08 11:25:42 -0600

    2
    2

---
+++ Guias de practica/Guia1.py
@@ -0,0 +1,20 @@
+nombre = "Ernesto"
+print(nombre)
+
+edad = 18
+ciudad = "San Salvador"
+profesion = "no lo se" 
+print(edad,ciudad,profesion)
+# una variable sirve para crear una lista de datos, guardarlos y usarlos despues
+newmsg = "Bienvenido al curso de Python"
+print(newmsg)
+anio_actual = 2025
+anio_nacimiento = 2006
+print(anio_actual-anio_nacimiento)
+Pi = 3.14159265359 
+print(f"El valor de Pi es:{Pi:.4f}")
+# una variable entera guarda números, una cadena texto
+
+#Sección 2
+
+usrname = input("Hola, ¿cuál es tu nombre?:" )
