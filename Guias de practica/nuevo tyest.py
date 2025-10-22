8f4e5ae54e3f4fe298d81aed636f574e4916cda1
20255816 <mediafire321ea@gmail.com>
2025-10-21 18:34:20 -0600

    Delete nuevo tyest.py

--- Guias de practica/nuevo tyest.py
+++
@@ -1,6 +0,0 @@
-
-
-nprom1, nprom2 = input("Dame 2 números sepadados por coma: ").split(",")
-nprom1 = int(nprom1)
-nprom2 = int(nprom2) 
-print(f"El promedio de esos números es: {(nprom1 + nprom2) / 2}" )
