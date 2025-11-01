bb5eba3b3c18278a23a02183e1466825aedbdc89
20255816 <mediafire321ea@gmail.com>
2025-10-31 21:33:13 -0600

    creo que ya lo termine

--- Guias de practica/idkbro.py
+++ Guias de practica/idkbro.py
@@ -1,36 +1,42 @@
+#configuracion inicial
 agente = 'encargado'
 platillo = []
 precios = []
 
-#1
+# Ingreso a la aplicacion
 name = input("Ingrese el nombre del agente: ")
 while name.lower() != agente:
     print("Agente no registrado")
     name = input("Favor ingrese el nombre del agente: ")
     
-#2
+# Gestion del menu princial
 OpcionesIniciadas = True
 
 while OpcionesIniciadas:
     print("Seleccione una opcion: 1.Creación de platillos.  2.Consulta de platillos y precios.  3.Colocar un pedido.  4.Salir")
     seleccion = int(input())
     
-    if seleccion == 4: # Salir
+    if seleccion == 4: # Salir 
         OpcionesIniciadas = False
         print("Gracias por usar este servicio!")
     elif seleccion == 1: #Creación de platillos
-        platillo.append(input("Ingrese el nombre del platillo a crear: "))
-        precios.append(float(input("Ingrese el precio del platillo a crear: ")))
+        platillo.append(input("Ingrese el nombre del platillo a crear: ").lower())
+        precios.append(float(input("Ingrese el precio del platillo a crear: ").lower()))
          
     elif seleccion == 2: #Consulta y precios
         if len(platillo) == 0:
             print("Actualmente no hay platillos ingresados")
         else:
-            for i in platillo:
-                print(i)
+            for i in range(len(platillo)):
+                print(f"{platillo[i]}: ${precios[i]:.2f}")
             
     elif seleccion == 3: #Colocar un pedido
-        ()
+        EleccionPedido = input("Indique el nombre del platillo para su orden: ")
+        if EleccionPedido.lower() in platillo:
+            number = platillo.index(EleccionPedido.lower())
+            print(f"Usted ha elegido {platillo[number]} con un precio de ${precios[number]:.2f}")
+        else:
+            print("Ese platillo no existe actualmente")
     else:
         print("Opción no válida")
     
